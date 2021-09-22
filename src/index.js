bddca813d50b86658d9b60ff349ead1913acf535
Andrii Lypovetskyi <li_andr@ukr.net>
2021-09-22 14:44:31 +0300

    000

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+// ============================================
+
+import Profile from './Profile';
+import user from './components/profile/user.json';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Profile
+    name={user.name}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+  />,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
