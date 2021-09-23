a8d6c9b0bafbd0da34558b5373a7be1cea3d0649
Andrii Lypovetskyi <li_andr@ukr.net>
2021-09-23 16:24:08 +0300

    Update FriendListItem.jsx

---
+++ src/components/friendListItem/FriendListItem.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+function FriendListItem({ isOnline, avatar, name }) {
+  return (
+    <li className="item">
+      <span className="status">{isOnline}</span>
+      <img className="avatar" src={avatar} alt={name} width="48" />
+      <p className="name">{name}</p>
+    </li>
+  );
+}
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string,
+  isOnline: PropTypes.bool,
+};
+export default FriendListItem;
