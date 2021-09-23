5545a7a2d0810868509c29bd43b0c8f2f160bed2
Andrii Lypovetskyi <li_andr@ukr.net>
2021-09-23 16:09:42 +0300

    rename files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Profile from './components/profile/Profile';
 import user from './components/profile/user.json';
-import Statistics from './components/statistic/Statistics';
-import statisticalData from './components/statistic/statistical-data.json';
+import Statistics from './components/statistics/Statistics';
+import statisticalData from './components/statistics/statistical-data.json';
 import FriendList from './components/friendList/FriendList';
 import friendsData from './components/friendList/friends.json';
 
